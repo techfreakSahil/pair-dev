c33cfe4973ac0750505cf4c25f28493b5990291a
techfreakSahil <gm6504@myamu.ac.in>
2024-04-25 20:33:32 +0530

    🚀 setting up docker and testing schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-// import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import * as schema from "./schema";
 import postgres from "postgres";
 
-// for migrations
-const migrationClient = postgres(
-  "postgres://postgres:adminadmin@0.0.0.0:5432/db",
-  { max: 1 }
-);
-// migrate(drizzle(migrationClient), ...)
-
 // for query purposes
-const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db");
-const db = drizzle(queryClient);
+const queryClient = postgres(process.env.DATABASE_URL!);
+const db = drizzle(queryClient, { schema });
 
 export { db };
-// await db.select().from(...)...
