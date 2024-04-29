578a3854c839aefaa526ed2b563416b8de70b6ef
techfreakSahil <gm6504@myamu.ac.in>
2024-04-29 21:48:24 +0530

    🚀ui minor changes

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,20 +1,52 @@
 "use client";
 import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
+import { LogOut, LogIn } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { signIn, signOut, useSession } from "next-auth/react";
+
+function AccountInfo() {
+  const session = useSession();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>{session?.data?.user?.name}</DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {session?.data ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOut /> Sign out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn("google")}>
+            <LogIn />
+            Sign in
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 export function Header() {
   const session = useSession();
 
   return (
-    <header>
-      <div>
-        {session?.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => signIn()}>Sign In</Button>
-        )}
-        <ModeToggle />
+    <header className="py-4 bg-gray-200 container mx-auto dark:bg-gray-900">
+      <div className="flex justify-between items-center">
+        <div>LOGO</div>
+        <div>
+          <AccountInfo />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
